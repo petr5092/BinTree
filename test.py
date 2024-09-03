318632e814af414584d7341b2340466aa5ac2922
PlayFish <136697820+petr5092@users.noreply.github.com>
2024-09-04 00:53:00 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 from bin_tree import *
+
+
 def test_1():
     lst = BinarySearchTree()
     for i in [10]:
@@ -14,12 +16,14 @@ def test_2():
     lst.delete(10)
     return lst.print_in_order()
 
+
 def test_3():
     lst = BinarySearchTree()
     for i in [10, 12]:
         lst.insert(i)
     lst.delete(10)
     return lst.print_in_order()
+
 
 def test_4():
     lst = BinarySearchTree()
@@ -28,6 +32,7 @@ def test_4():
     lst.delete(10)
     return lst.print_in_order()
 
+
 def test_5():
     lst = BinarySearchTree()
     for i in [10, 5, 12, 6, 3]:
@@ -35,12 +40,14 @@ def test_5():
     lst.delete(5)
     return lst.print_in_order()
 
+
 def test_6():
     lst = BinarySearchTree()
     for i in [10, 5, 13, 12, 11]:
         lst.insert(i)
     lst.delete(10)
     return lst.print_in_order()
+
 
 def test_7():
     lst = BinarySearchTree()
@@ -57,12 +64,14 @@ def test_8():
     lst.delete(10)
     return lst.hight_tree()
 
+
 def test_9():
     lst = BinarySearchTree()
     for i in [10, 5, 13, 12, 11, 2, 1, 3]:
         lst.insert(i)
     lst.delete(3)
     return lst.width_tree()
+
 
 def test_control():
     print(test_1() == None)
@@ -74,5 +83,6 @@ def test_control():
     print(test_7() == 7)
     print(test_8() == 3)
     print(test_9() == 2, test_9())
+
 
 test_control()
